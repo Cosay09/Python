e0334aea343d866b41a919970cfbe111ced8b9cf
Saydul Islam <childrengang8@gmail.com>
2025-04-23 17:51:55 +0600

    Updatae stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -62,3 +62,28 @@ int main() {
     cout << endl;
     return 0;
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+int ack(int m, int n)
+{
+    if (m == 0)
+        return n+1;
+    else if (m > 0 && n == 0)
+        return ack(m-1, 1);
+    else if (m > 0 && n > 0)
+        return ack(m-1, ack(m, n - 1));
+}
+
+int main()
+{
+    int m, n; 
+    cin >> m >> n;
+
+    cout << ack(m, n);
+
+    return 0;
+}
+*/
