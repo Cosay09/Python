2ce6db9e45c373420b172109053fc3e941a46ee2
Saydul Islam <childrengang8@gmail.com>
2025-04-23 17:34:00 +0600

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
-#include <cstdlib>
+#include <cctype>
 using namespace std;
 
 char stack[100];
 int top = -1;
 
-void push(char x)
-{
-    stack[top++] = x;
+void push(char x) {
+    stack[++top] = x;
 }
 
-char pop()
-{
+char pop() {
     if (top == -1)
-        return -1;
-    else
-        stack[top--];
+        return '\0'; // safer than -1 for char
+    return stack[top--];
 }
 
-int priority(char x)
-{
+int priority(char x) {
     if (x == '(')
         return 0;
     else if (x == '+' || x == '-')
         return 1;
     else if (x == '*' || x == '/')
         return 2;
-
     return 0;
 }
 
-int main()
-{
+int main() {
     char exp[100];
     char *e, x;
 
@@ -40,30 +34,31 @@ int main()
 
     e = exp;
 
-    while(*e != '\0')
-    {
-        if( isalnum(*e) )
+    while (*e != '\0') {
+        if (isalnum(*e)) {
             cout << *e;
-        else if (*e == '(')
+        }
+        else if (*e == '(') {
             push(*e);
-        else if (*e == ')')
-        {
-            while((x = pop()) != '(')
+        }
+        else if (*e == ')') {
+            while ((x = pop()) != '(') {
                 cout << x;
-        }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
-            {
-                cout << pop();
-                push(*e);
             }
-            e++;
         }
+        else {
+            while (priority(stack[top]) >= priority(*e)) {
+                cout << pop();
+            }
+            push(*e);
+        }
+        e++;
     }
-    while(top != -1)
-    {
+
+    while (top >= 0) {
         cout << pop();
     }
-    return 0;;
+
+    cout << endl;
+    return 0;
 }
